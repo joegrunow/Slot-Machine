6deece4fdc222abaf57fcee31fa6b798261ae7b9
joegrunow <131898008+joegrunow@users.noreply.github.com>
2023-10-25 13:32:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Slot Machine!</h1>
+                <Machine 
+                    s1="🧀"
+                    s2="🧀"
+                    s3="🧀"
+                />
+                <Machine 
+                    s1="🍗"
+                    s2="🧀"
+                    s3="🧀"
+                />
+                <Machine 
+                    s1="🍺"
+                    s2="🧀"
+                    s3="🍺"
+                />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
